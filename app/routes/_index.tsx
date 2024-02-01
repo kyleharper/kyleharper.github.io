4ec5a49b60e5c031eb0fa8e93ff83d5172c43715
Kyle Harper <kyleharperllama@gmail.com>
2024-02-01 23:27:58 +0000

    Move static content to remix index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,31 +2,19 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix SPA" },
-    { name: "description", content: "Welcome to Remix (SPA Mode)!" },
+    { title: "Kyle Harper" },
+    { name: "description", content: "Kyle Harper, Product Engineer" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix (SPA Mode)</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/future/spa-mode"
-            rel="noreferrer"
-          >
-            SPA Mode Guide
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="h-screen flex items-center justify-center">
+      <p className="text-xl tracking-tight">
+        Kyle Harper, Product Engineer & Engineering Manager <a
+          className="text-blue-600 dark:text-blue-500 hover:underline" href="https://www.bynder.com/en/">@Bynder</a>.
+        😊
+      </p>
     </div>
   );
 }
