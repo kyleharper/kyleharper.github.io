e8674a3c9231ecddf81a73e8e0a93551263bec18
Kyle Harper <kyleharperllama@gmail.com>
2024-02-01 23:29:38 +0000

    Add tailwind build steps and ensure use of tailwind in Remix SPA

--- app/root.tsx
+++ app/root.tsx
@@ -7,39 +7,44 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import "~/tailwind-o.css";
+
 export default function App() {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
+    <head>
+        <meta charSet="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <Meta/>
+        <Links/>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="use-credentials" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet"/>
+    </head>
+    <body className="font-sans">
+        <Outlet/>
+        <ScrollRestoration/>
+        <Scripts/>
+        <LiveReload/>
+    </body>
     </html>
   );
 }
 
 export function HydrateFallback() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <p>Loading...</p>
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <head>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <Meta/>
+            <Links/>
+        </head>
+        <body className="font-sans">
+            <Scripts/>
+            <LiveReload/>
+        </body>
+        </html>
+    );
 }
