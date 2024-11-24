1f0bc428cded5b482c0f8a9dc8797679a5b07847
kyleharper <kyleharperllama@gmail.com>
2024-11-24 13:51:53 +0000

    add "Sync Engines, Part One" post.

--- app/data/blog.ts
+++ app/data/blog.ts
@@ -1,8 +1,9 @@
 export type Post = {
-	title: string;
-	date: string;
-	intro: string;
-	filepath: string;
+  title: string;
+  date: string;
+  intro: string;
+  filepath: string;
 };
 
+export * as SyncEnginesPartOne from "../routes/blog.sync-sngines-part-one.mdx";
 export * as ThoughtsOfEngMngPost from "../routes/blog.thoughts-on-engineering-management.mdx";
